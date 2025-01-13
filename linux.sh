8ee18b356ff86f3bf9ed910f2a54711a5535402c
perdub <81479253+perdub@users.noreply.github.com>
2025-01-14 01:36:27 +0300

    Update linux.sh to clang 18 and ubuntu 24

--- linux.sh
+++ linux.sh
@@ -25,15 +25,28 @@
 #cd ../..
 #ls -l telegram-bot-api*
 
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-14 libc++-dev libc++abi-dev
+#git clone --recursive https://github.com/tdlib/telegram-bot-api.git
+##cd telegram-bot-api
+#rm -rf build
+#mkdir build
+#cd build
+#CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang-14 CXX=/usr/bin/clang++-14 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
+#cmake --build . --target install
+#cd ../..
+#ls -l telegram-bot-api/bin/telegram-bot-api*
+
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-14 libc++-dev libc++abi-dev
+sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-18 libc++-18-dev libc++abi-18-dev
 git clone --recursive https://github.com/tdlib/telegram-bot-api.git
 cd telegram-bot-api
 rm -rf build
 mkdir build
 cd build
-CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang-14 CXX=/usr/bin/clang++-14 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
+CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang-18 CXX=/usr/bin/clang++-18 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
 cmake --build . --target install
 cd ../..
 ls -l telegram-bot-api/bin/telegram-bot-api*
